007d3c088b4454b0036872af9c9e07cee2b563fd
Dzmitry Mishchanka <dzmitry.mishchanka@gmail.com>
2023-01-15 22:10:54 +0100

    Implement MongoDB connection
    Implemented MongoDB connection and user saving

--- bot.py
+++ bot.py
@@ -1,29 +1,35 @@
 import logging
-import os
-import random
+from asyncio import get_event_loop, new_event_loop, set_event_loop
 
-from telegram import Update, ChatMember
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+from config import config
+from database import database, mongodb
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+logger = logging.getLogger("__name__")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Privet!')
+try:
+    loop = get_event_loop()
+except RuntimeError:
+    set_event_loop(new_event_loop())
+    loop = get_event_loop()
 
-async def select_aboba(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='@' + user.username)
+loop.run_until_complete((mongodb.check_connection(config.MONGO_URI)))
+
+
+async def save_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await database.save_user(update.effective_user)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Я сохранил тебя в базу данных!")
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(
-        os.environ.get('TELEGRAM_TOKEN')).build()
-    start_handler = CommandHandler('start', start)
-    aboba_handler = CommandHandler('select_aboba', select_aboba)
+    application = ApplicationBuilder().token(config.TELEGRAM_TOKEN).build()
+    start_handler = CommandHandler('start', save_user)
     application.add_handler(start_handler)
-    application.add_handler(aboba_handler)
     application.run_polling()
---
+++ config/config.py
@@ -0,0 +1,8 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN")
+MONGO_URI = getenv("MONGO_URI")
