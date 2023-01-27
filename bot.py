f73f81f869ef7071d37c603576686f2e0606935e
Dzmitry Mishchanka <dzmitry.mishchanka@gmail.com>
2023-01-27 23:04:45 +0100

    Implement Beanie ODM

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
+import datetime
 import logging
 from asyncio import get_event_loop, new_event_loop, set_event_loop
+from datetime import datetime, timezone
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 from bot.config import config
-from bot.database import mongo, mongo_users
+from bot.database import mongo
+from bot.models.chat_user import ChatUser
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,6 +24,7 @@ except RuntimeError:
     loop = get_event_loop()
 
 loop.run_until_complete((mongo.check_connection(config.MONGO_URI)))
+loop.run_until_complete((mongo.initialize()))
 
 
 async def start_greetings(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -28,36 +32,19 @@ async def start_greetings(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
 
 async def register_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await mongo_users.save_user(update.effective_user)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Я сохранил тебя в базу данных')
-
-
-async def find_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        user_id = int(context.args[0])
-        user = await mongo_users.find_user(user_id)
-        if not user:
-            await update.message.reply_text('Пользователь с таким ID не найден')
-        else:
-            await update.message.reply_text(str(user))
-    except (IndexError, ValueError):
-        await update.message.reply_text('Необходимо указать ID пользователя!')
-
-
-async def get_all_user_ids(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_ids = await mongo_users.get_all_user_ids()
-    joined_user_ids = ','.join(map(str, user_ids))
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Все пользователи: ' + joined_user_ids)
+    user = update.effective_user
+    chat = update.effective_chat
+    logger.info(user)
+    chat_user = ChatUser(user_id=user.id, chat_id=chat.id, username=user.username,
+                         first_name=user.first_name, last_name=user.last_name, last_update=datetime.now(timezone.utc))
+    await chat_user.insert()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Пользователь зарегистрирован: ' + str(chat_user))
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(config.TELEGRAM_TOKEN).build()
     start_handler = CommandHandler('start', start_greetings)
     register_user_handler = CommandHandler('register', register_user)
-    get_all_user_ids_handler = CommandHandler('list_users', get_all_user_ids)
-    find_user_handler = CommandHandler('find_user', find_user)
     application.add_handler(start_handler)
     application.add_handler(register_user_handler)
-    application.add_handler(get_all_user_ids_handler)
-    application.add_handler(find_user_handler)
     application.run_polling()
--- bot/database/mongo.py
+++ bot/database/mongo.py
@@ -1,45 +1,11 @@
 import logging
 import sys
 
-from bot.config import config
+from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
-
-class MongoCollection:
-
-    def __init__(self, collection):
-        self.collection = collection
-
-    async def find(self, document_id):
-        """
-        Find document by document ID.
-        """
-        return await self.collection.find_one({"_id": document_id})
-
-    async def update(self, document_id, document_data):
-        """
-        Update or create document by document ID.
-        """
-        document_data = {"$set": document_data}
-        await self.collection.update_one({"_id": document_id}, document_data, upsert=True)
-
-    async def delete(self, document_id):
-        """
-        Delete document by document ID.
-        """
-        await self.collection.delete_one({'_id': document_id})
-
-    async def count(self):
-        """
-        Get total number of documents in this collection.
-        """
-        return await self.collection.count_documents({})
-
-    async def get_all_ids(self):
-        """
-        Get list of all document IDs in this collection.
-        """
-        return await self.collection.distinct("_id")
+from bot.config import config
+from bot.models.chat_user import ChatUser
 
 
 async def check_connection(uri: str) -> None:
@@ -52,8 +18,7 @@ async def check_connection(uri: str) -> None:
             "Can't connect to MongoDB: invalid URI")
         sys.exit()
 
-mongo = AsyncIOMotorClient(config.MONGO_URI)
-database = mongo.bot
 
-users = MongoCollection(database.users)
-chats = MongoCollection(database.chats)
+async def initialize() -> None:
+    client = AsyncIOMotorClient(config.MONGO_URI)
+    await init_beanie(database=client.bot, document_models=[ChatUser])
