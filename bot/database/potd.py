209a1c2d548d9c955923598b0c714d519b5313bf
4808_5 <andrey.zakharow@keemail.me>
2023-01-28 20:46:14 +0100

    fixed registered users check

--- bot/database/potd.py
+++ bot/database/potd.py
@@ -27,5 +27,25 @@ class PersonOfTheDayGame(Document):
     last_play_date: datetime
     creation_date: datetime
 
+    def is_player_exist(self, user_id: int) -> bool:
+        return any(player.id == user_id for player in self.players)
+
+    async def add_player(self, user) -> bool:
+        player_added = False
+        if not self.is_player_exist(user.id):
+            potd_player: PersonOfTheDayPlayer = PersonOfTheDayPlayer(id =           user.id, 
+                                                                    username =      user.username, 
+                                                                    first_name =    user.first_name, 
+                                                                    last_name =     user.last_name, 
+                                                                    wins_number =   0)
+            self.players.append(potd_player)
+            await self.save()
+            player_added = True
+        return player_added
+
+    def get_player_list(self) -> List:
+        return self.players
+
+
     class Settings:
         name = "potd"
